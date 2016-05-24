da3167932b2ef4e1585ddb35a425b9bc8b8d94fb
Marc Solèr <Marc Solr>
2016-05-24 19:45:04 +0200

    Removes unnecessary jQuery (uses WP lib)

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-$(function () {
+jQuery(document).ready(function($) {
     // Responsive navigation
     $('#menu-spinner-button').click(function () {
         $('#sub-nav-collapse').slideToggle();
     });
 });
-
